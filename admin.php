1b2a3bd2baaef1cb9e9834f5d83c2541a1344075
Dilshard <mrdilshard@gmail.com>
2024-03-20 12:31:44 +0530

    Dashboard cards

--- admin.php
+++ admin.php
@@ -4,38 +4,42 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12 nav-space">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-                    <div class="container-fluid">
-                      <a class="navbar-brand" href="#">WebMaster</a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                      </button>
-                      <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                          <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">Student</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">Staff</a>
-                          </li>
-                          <li class="nav-item">
-                            <a class="nav-link" href="#">Student complains</a>
-                          </li>
-                        </ul>
-                        <span class="navbar-text">
-                          <button class="btn btn-danger">Sign out</button>
-                        </span>
-                      </div>
-                    </div>
-                  </nav>
-            </div>
+            
         </div>
         <div class="row">
-          
+          <div class="col-md-4 p-4">
+            <div class="card" style="width: 100%;">
+              <div class="card-body bg-danger text-light">
+                <h5 class="card-title">September Intake</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-light">2023</span></h6>
+                <p class="card-text display-1 fw-bold">523</p>
+                <a href="#" class="card-link text-light">List</a>
+                <a href="#" class="card-link text-light">Projects</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 p-4">
+          <div class="card" style="width: 100%;">
+              <div class="card-body bg-primary text-light">
+                <h5 class="card-title">Students without supervisor</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-light">2023</span></h6>
+                <p class="card-text display-1 fw-bold">21</p>
+                <a href="#" class="card-link text-light">List</a>
+                <a href="#" class="card-link text-light">Projects</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 p-4">
+          <div class="card" style="width: 100%;">
+              <div class="card-body bg-warning text-dark">
+                <h5 class="card-title">Project completion rate</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-light">2023</span></h6>
+                <p class="card-text display-1 fw-bold">33%</p>
+                <a href="#" class="card-link text-light">List</a>
+                <a href="#" class="card-link text-light">Projects</a>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
