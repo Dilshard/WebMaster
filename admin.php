b6455329b3c0ce49bc18010d2fbd0e2700a58ce8
Dilshard <mrdilshard@gmail.com>
2024-03-20 12:10:00 +0530

    Nav bar complete

--- admin.php
+++ admin.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <?php include("head.php"); ?>
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="col-md-12 nav-space">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                     <div class="container-fluid">
-                      <a class="navbar-brand" href="#">Navbar w/ text</a>
+                      <a class="navbar-brand" href="#">WebMaster</a>
                       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                       </button>
@@ -17,20 +17,29 @@
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Student</a>
                           </li>
                           <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" href="#">Staff</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Student complains</a>
                           </li>
                         </ul>
                         <span class="navbar-text">
-                          Navbar text with an inline element
+                          <button class="btn btn-danger">Sign out</button>
                         </span>
                       </div>
                     </div>
                   </nav>
             </div>
         </div>
+        <div class="row">
+          
+        </div>
     </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+      crossorigin="anonymous"></script>
 </body>
 </html>
